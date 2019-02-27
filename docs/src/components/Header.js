73869a23fabd33de565081a47d5241942df4fcd1
johnpolacek <johnpolacek@gmail.com>
2019-02-26 20:22:39 -0600

    update styled-system to 4.0.0

--- docs/src/components/Header.js
+++ docs/src/components/Header.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Div, H1, P, A } from 'styled-system-html'
-import Button from './Button'
 
 const Header = props =>
 	<Div is='header'>
-		<Div mx="auto" css={{maxWidth:'960px'}} pt={4} pb={2}>
+		<Div mx="auto" bg="blue0" py={4}>
 			<Div width={1} textAlign="center">
-				<H1 fontSize={6} pb={1}>&lt;/&gt; Styled System HTML</H1>
-				<P>HTML elements extended as components, ready for theming via styled system.</P>
-				<P mt={-2} pb={3}>Built with/for <A color="blue" fontWeight="bold" href="http://styled-components.com">Styled Components</A> &amp; <A color="blue" fontWeight="bold" href="http://jxnblk.com/styled-system/">Styled System</A></P>
-				<Button fontSize={2} is="a" href="https://github.com/johnpolacek/styled-system-html/" bg='blue'>View on Github</Button>
+				<H1 fontSize={5} fontWeight="normal" fontFamily='"Lucida Console", Monaco, monospace'><Div display="inline-block" borderRadius="8px" border="1px solid" p={2} fontSize={4} position="relative" top="-2px">&lt;/&gt;</Div> Styled System HTML</H1>
+				<A fontSize={3} fontWeight="bold " href="https://github.com/johnpolacek/styled-system-html/" color='blue'>View on Github</A>
+				<P pt={4}>HTML elements extended as components, ready for theming via styled system.</P>
+				<P mt={-2}>Built with/for <A color="blue" fontWeight="bold" href="http://styled-components.com">Styled Components</A> &amp; <A color="blue" fontWeight="bold" href="http://jxnblk.com/styled-system/">Styled System</A></P>
 			</Div>
 		</Div>
 	</Div>
